4f7b01a0407c8d602e0d60c8e505766ea01c5ba3
Alec Wells <alec.wells47@gmail.com>
2019-11-21 13:51:41 -0700

    Create favorite functions to return weather forecast for each location

--- routes/api/v1/favorites.js
+++ routes/api/v1/favorites.js
@@ -1,10 +1,15 @@
 var express = require('express');
+var forecast = require('./forecast');
 var router = express.Router();
 
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('../../../knexfile')[environment];
 const database = require('knex')(configuration);
 const fetch = require('node-fetch');
+
+const helpers = require('../../../application_helper/helper_functions');
+const fetchForecast = helpers.fetchForecast;
+
 
 async function getUser(key) {
   let users = await database('users').where('api_key', key);
@@ -19,11 +24,39 @@ async function getFavorites(key) {
   return locations;
 }
 
+const getForecasts = (locations) => {
+  const promises = locations.map(async (location) => {
+      return {
+          location: `${location}`,
+          forecast: await fetchForecast(location)
+      }
+  });
+  return Promise.all(promises);
+}
+
+async function getFavForecast(key) {
+  let locations = await getFavorites(key);
+  let forecasts = await getForecasts(locations)
+  // might need to make anon async function within map
+  // let forecasts = locations.map(x => await fetchForecast(x));
+
+  console.log(forecasts)
+  // return forecasts;
+}
+// async function getFavForecast(key) {
+//   let locations = await getFavorites(key);
+//   // might need to make anon async function within map
+//   let forecasts = locations.map(x => await fetchForecast(x));
+//   console.log(forecasts)
+//   return forecasts;
+// }
 
 // Might need to add .select() to the end of the query?
 router.get('/', (request, response) => {
-  getFavorites(request.body.api_key)
+  // getFavorites(request.body.api_key)
+  getFavForecast(request.body.api_key)
 });
+
 
 // Could create const all = () => database('papers')
 //   .select()
