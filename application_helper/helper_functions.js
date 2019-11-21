70edf7052d33b033c6303b7c4f60971057ad1ab2
Alec Wells <alec.wells47@gmail.com>
2019-11-21 07:53:13 -0700

    Move async forecast functions to helper_functions

--- application_helper/helper_functions.js
+++ application_helper/helper_functions.js
@@ -1,8 +1,5 @@
-var express = require('express');
-var router = express.Router();
-
 const environment = process.env.NODE_ENV || 'development';
-const configuration = require('../../../knexfile')[environment];
+const configuration = require('../knexfile')[environment];
 const database = require('knex')(configuration);
 const fetch = require('node-fetch');
 
@@ -25,25 +22,8 @@ async function fetchForecast(address) {
   return forecast;
 };
 
-// Might need to add .select() to the end of the query?
-router.get('/', (request, response) => {
-  database('users').where('api_key', request.body.api_key)
-    .then(users => {
-      if (users.length) {
-        fetchForecast(request.query)
-          .then(data => response.status(200).send(data))
-          .catch(reason => response.send(reason.message))
-      } else {
-        response.status(401).json({
-          error: `Unauthorized, Could not find user with api key: ${request.body.api_key}`
-        });
-      }
-    })
-    .catch(error => {
-      response.status(500).json({ error });
-    });
-});
-
-// Could create const all = () => database('papers')
-//   .select()
-module.exports = router
+module.exports = {
+  fetchLocation: fetchLocation,
+  coordinates: coordinates,
+  fetchForecast: fetchForecast
+}
