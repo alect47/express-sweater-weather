8d11cea0f11e5262d7d5394d7acabc195fa7b640
Alec Wells <alec.wells47@gmail.com>
2019-11-21 07:54:01 -0700

    Refactor forecast to use helper methods

--- routes/api/v1/forecast.js
+++ routes/api/v1/forecast.js
@@ -6,31 +6,36 @@ const configuration = require('../../../knexfile')[environment];
 const database = require('knex')(configuration);
 const fetch = require('node-fetch');
 
-async function fetchLocation(address) {
-  let response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${address.location}&key=${process.env.GOOGLE_API_KEY}`);
-  let location = await response.json();
-  return location;
-}
-
-async function coordinates(address) {
-  let data = await fetchLocation(address)
-  let coords = data.results[0].geometry.location;
-  return coords;
-};
-
-async function fetchForecast(address) {
-  let coords = await coordinates(address);
-  let response = await fetch(`https://api.darksky.net/forecast/${process.env.DARK_SKY_API}/${coords.lat},${coords.lng}`);
-  let forecast = await response.json();
-  return forecast;
-};
+const helpers = require('../../../application_helper/helper_functions');
+// const fetchLocationH = helpers.fetchLocation;
+// const coordinatesH = helpers.coordinates;
+const fetchForecastH = helpers.fetchForecast;
+//
+// async function fetchLocation(address) {
+//   let response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${address.location}&key=${process.env.GOOGLE_API_KEY}`);
+//   let location = await response.json();
+//   return location;
+// }
+//
+// async function coordinates(address) {
+//   let data = await fetchLocation(address)
+//   let coords = data.results[0].geometry.location;
+//   return coords;
+// };
+//
+// async function fetchForecast(address) {
+//   let coords = await coordinates(address);
+//   let response = await fetch(`https://api.darksky.net/forecast/${process.env.DARK_SKY_API}/${coords.lat},${coords.lng}`);
+//   let forecast = await response.json();
+//   return forecast;
+// };
 
 // Might need to add .select() to the end of the query?
 router.get('/', (request, response) => {
   database('users').where('api_key', request.body.api_key)
     .then(users => {
       if (users.length) {
-        fetchForecast(request.query)
+        fetchForecastH(request.query)
           .then(data => response.status(200).send(data))
           .catch(reason => response.send(reason.message))
       } else {
@@ -46,4 +51,4 @@ router.get('/', (request, response) => {
 
 // Could create const all = () => database('papers')
 //   .select()
-module.exports = router;
+module.exports = router
