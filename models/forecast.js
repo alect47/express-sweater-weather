2fb6a010455a883d49f1b8ed5036041ce36c83fe
Alec Wells <alec.wells47@gmail.com>
2019-11-22 06:56:10 -0700

    Finish forecast model

--- models/forecast.js
+++ models/forecast.js
@@ -2,7 +2,7 @@ class Forecast {
   // Think of a constructor as being similar to initialize in Ruby
   // Instead of setting attributes like @name = name in JavaScript we say this.name = name
   constructor(location, forecast) {
-    this.location = location;
+    this.location = location.location;
     this.currently = forecast.currently;
     this.hourly = forecast.hourly;
     this.daily = forecast.daily;
